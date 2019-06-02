d03989116e85b6b7df18bed6f7e2a4c462e1f14e
NVlad <wildvampir@gmail.com>
2019-06-03 02:52:37 +0300

    add link to https://tinypng.com/developers page

--- src/main/java/com/nvlad/tinypng/Constants.java
+++ src/main/java/com/nvlad/tinypng/Constants.java
@@ -2,7 +2,7 @@ package com.nvlad.tinypng;
 
 public interface Constants {
     String TITLE = "TinyPNG Image Optimizer";
-    String API_KEY_QUESTION = "What's your TinyPNG API Key?";
+    String API_KEY_QUESTION = "<html>What's your <a href=\"https://tinypng.com/developers\">TinyPNG API</a> Key?</html>";
     String SETTINGS_USAGE = "Usage this month: ";
     String SETTINGS_USAGE_EMPTY = "Usage this month: ---";
 }
